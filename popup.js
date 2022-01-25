8a5d99594562aea38c927cde7cbe31668b72f0db
ccarmeli <coral.carmeli@intel.com>
2022-01-25 15:04:41 +0200

    table project was added

--- popup.js
+++ popup.js
@@ -13,7 +13,7 @@ openExtensionButton.addEventListener("click", async () => {
   //   target: { tabId: tab.id },
   //   function: setPageBackgroundColor,
   // });
-  var newURL = "file://ger/ec/proj/ha/mdo/mpv/aig/Users/ccd/table-01/index.html";
+  var newURL = "file:///C:/Users/ccarmeli/ccd/AmazonFreeShippingTracker-2/table-01/index.html";
   chrome.tabs.create({ url: newURL });
 });
 
